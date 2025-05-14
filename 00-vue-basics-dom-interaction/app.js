e1eeb213391d655c5a6e2f9f6ef5215f3ce1201a
Cristian  Murillo <cam_725@hotmail.com>
2025-05-14 23:56:15 +0200

    used functions and defined methods in the tenmplate

--- 00-vue-basics-dom-interaction/app.js
+++ 00-vue-basics-dom-interaction/app.js
@@ -1,10 +1,16 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoal: 'Finish this course and learn Vue!',
-            vueLink: 'https://vuejs.org/'
-        };
+  data: () => {
+    return {
+      courseGoal: "Finish this course and learn Vue!",
+      vueLink: "https://vuejs.org/"
+    };
+  },
+  methods: {
+    outputGoal: () => {
+      const randomNumber = Math.random();
+      return randomNumber < 0.5 ? "Learn Vue!" : "Master Vue!";
     }
+  }
 });
 
-app.mount('#user-goal');
+app.mount("#user-goal");
