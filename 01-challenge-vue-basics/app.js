f9e62ff811015d9347824ea7baaaa0b858eb2a2d
Cristian  Murillo <cam_725@hotmail.com>
2025-05-16 23:50:54 +0200

    resolved  watchers challenge

---
+++ 01-challenge-vue-basics/app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+  data: function () {
+    return {
+      secret: 37,
+      counter: 0,
+      lastTimeOut: null
+    };
+  },
+  watch: {
+    result(value) {
+      if (this.lastTimeOut) clearTimeout(this.lastTimeOut);
+      this.lastTimeOut = setTimeout(() => {
+        this.counter = 0;
+      }, 5000);
+    }
+  },
+  computed: {
+    result() {
+      if (this.counter === 0) return "";
+      if (this.counter < this.secret) return "Not there yet";
+      if (this.counter > this.secret) return "Too much!";
+      return `${this.secret}`;
+    }
+  },
+  methods: {
+    increase: function (step) {
+      this.counter += step;
+      console.log(this.counter);
+    },
+    reset: function () {
+      return setTimeout(() => {
+        if (this.counter === 0) return;
+        this.counter = 0;
+        console.log("changed ", this.counter);
+      }, 5000);
+    }
+  }
+});
+
+app.mount("#assignment");
