6f6bdb2dd42138d615d79b6c08f6fbc2f95bb020
Cristian  Murillo <cam_725@hotmail.com>
2025-05-16 08:35:03 +0200

    added vue event modifiers

--- 00-vue-basics-dom-interaction/app.js
+++ 00-vue-basics-dom-interaction/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
-      id: ""
+      id: "",
+      confirmedName: ""
     };
   },
   methods: {
@@ -13,11 +14,16 @@ const app = Vue.createApp({
     decreaseCounter: function (step) {
       if (this.counter > 0) this.counter -= step ?? 0;
     },
+    // used a different sintax here (just to keep in mind it's also valid)
     setName(event) {
       this.name = event.target.value;
     },
     setId(event, preffix) {
       this.id = event.target.value ? `${preffix}${event.target.value}` : "";
+    },
+    submitForm: function () {},
+    confirmInput: function () {
+      this.confirmedName = this.name;
     }
   }
 });
