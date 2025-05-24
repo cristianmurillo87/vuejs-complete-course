ba209d285eeb4441db3dbf2547e6573581edb9ca
Cristian  Murillo <cam_725@hotmail.com>
2025-05-24 12:01:52 +0200

    introduced the concept of custom components

---
+++ 05-intro-to-components-and-vue-cli/vue-intro-components/src/components/FriendContact.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data: function () {
+    return {
+      showDetails: false,
+      isContactFavorite: this.isFavorite
+    };
+  },
+  methods: {
+    toggleDisplayDetails: function () {
+      this.showDetails = !this.showDetails;
+    },
+    toggleIsFavorite: function () {
+      this.isContactFavorite = !this.isContactFavorite;
+    }
+  }
+};
+</script>
+<template>
+  <li>
+    <h2>{{ name }} {{ isContactFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleIsFavorite">
+      {{ isContactFavorite ? "Unmark" : "Mark" }} as favorite
+    </button>
+    <button @click="toggleDisplayDetails">
+      {{ showDetails ? "Hide" : "Show" }} Details
+    </button>
+
+    <ul v-if="showDetails">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
