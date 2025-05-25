48122999c3bb099d0fe5b249a0b24650cf1664c6
Cristian  Murillo <cam_725@hotmail.com>
2025-05-25 14:32:33 +0200

    used keep-alive component for preventing an element to be deleted from the DOM when using v-if

--- 06-deep-dive-into-components/src/App.vue
+++ 06-deep-dive-into-components/src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <component :is="selectedComponent"></component>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
