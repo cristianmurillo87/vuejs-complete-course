010f9ed531f4f5f3773b06d45ec1324229106efa
Cristian  Murillo <cam_725@hotmail.com>
2025-05-25 14:52:34 +0200

    used teleport to define the element where a component should be rendered

--- 06-deep-dive-into-components/src/components/ManageGoals.vue
+++ 06-deep-dive-into-components/src/components/ManageGoals.vue
@@ -1,3 +1,41 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <!-- teleport is used to display the contained element into the element defined
+      in the "to" property; which accepts css selector. The element will be displayed
+      inside the first element that satisfies the passed selector. 
+    -->
+    <teleport to="body">
+      <error-alert v-if="isInputInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter a goal...</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </teleport>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert
+  },
+  data: function () {
+    return {
+      isInputInvalid: false
+    };
+  },
+  methods: {
+    setGoal: function () {
+      const value = this.$refs.goal.value;
+      this.isInputInvalid = value === "";
+    },
+    confirmError: function () {
+      this.isInputInvalid = false;
+    }
+  }
+};
+</script>
