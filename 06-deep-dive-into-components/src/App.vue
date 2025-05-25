60666c82dff2de34ee87441255ee50a5ca9f08e2
Cristian  Murillo <cam_725@hotmail.com>
2025-05-25 14:17:53 +0200

    used scoped slots for rendering of multiple components

--- 06-deep-dive-into-components/src/App.vue
+++ 06-deep-dive-into-components/src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps["another-property"] }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -16,11 +22,14 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
 export default {
   components: {
     "app-header": TheHeader,
     "badge-list": BadgeList,
-    "user-info": UserInfo
+    "user-info": UserInfo,
+    "course-goals": CourseGoals
   },
   data() {
     return {
---
+++ 06-deep-dive-into-components/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-property="..."></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      goals: ["Finish the course", "Learn Vue"]
+    };
+  }
+};
+</script>
