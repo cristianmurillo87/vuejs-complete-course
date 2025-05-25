aca14eb06bcd5792572ceb2fa20dc6c1e73319d2
Cristian  Murillo <cam_725@hotmail.com>
2025-05-25 13:57:55 +0200

    implemented conditional rendering of slots

--- 06-deep-dive-into-components/src/components/BaseCard.vue
+++ 06-deep-dive-into-components/src/components/BaseCard.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <header>
+    <header v-if="$slots['card-header']">
+      <!-- named slot-->
       <slot name="card-header"></slot>
     </header>
+    <!-- default slot-->
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  mounted: function () {
+    // checking whether something has been added to the
+    // card-header slot in the template
+    console.log(this.$slots["card-header"]);
+  }
+};
+</script>
 
 <style scoped>
 header {
