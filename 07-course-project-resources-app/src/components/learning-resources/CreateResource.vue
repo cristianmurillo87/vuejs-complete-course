11b0d2fc01ac54e93baf6e8d3357effbdda16577
Cristian  Murillo <cam_725@hotmail.com>
2025-05-26 19:46:47 +0200

    used computed values to determine the active tab

---
+++ 07-course-project-resources-app/src/components/learning-resources/CreateResource.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Create Resource</h2>
+</template>
---
+++ 07-course-project-resources-app/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,64 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResourcesButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('create-resource')"
+      :mode="createResourceButtonMode"
+      >Create Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from "./StoredResources.vue";
+import CreateResource from "./CreateResource.vue";
+
+export default {
+  components: {
+    StoredResources,
+    CreateResource
+  },
+  data: function () {
+    return {
+      selectedTab: "stored-resources",
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org"
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com"
+        }
+      ]
+    };
+  },
+  provide: function () {
+    return {
+      resources: this.storedResources
+    };
+  },
+  computed: {
+    storedResourcesButtonMode: function () {
+      return this.selectedTab === "stored-resources" ? null : "flat";
+    },
+    createResourceButtonMode: function () {
+      return this.selectedTab === "create-resource" ? null : "flat";
+    }
+  },
+  methods: {
+    setSelectedTab: function (tab) {
+      this.selectedTab = tab;
+    }
+  }
+};
+</script>
