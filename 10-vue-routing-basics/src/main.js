45442e4970d40cbfb64f29103be0c084b9281447
Cristian  Murillo <cam_725@hotmail.com>
2025-06-02 20:25:09 +0200

    integrateed view routing for single page application navigation

--- 10-vue-routing-basics/src/main.js
+++ 10-vue-routing-basics/src/main.js
@@ -1,7 +1,35 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import App from './App.vue';
+import App from "./App.vue";
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/users/UsersList.vue";
+import TeamMembers from "./components/teams/TeamMembers.vue";
 
-const app = createApp(App)
+const router = createRouter({
+  // browser built-in history support
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      redirect: "/teams"
+    },
+    {
+      path: "/teams",
+      component: TeamsList
+    },
+    {
+      path: "/users",
+      component: UsersList
+    },
+    {
+      path: "/teams/:teamId",
+      component: TeamMembers
+    }
+  ]
+});
 
-app.mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
