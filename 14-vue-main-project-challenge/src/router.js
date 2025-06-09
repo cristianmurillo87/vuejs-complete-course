3313fdd196f5e7750337941b1d8855454419abf9
Cristian  Murillo <cam_725@hotmail.com>
2025-06-09 19:34:58 +0200

    set up application routes and base components

---
+++ 14-vue-main-project-challenge/src/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachesList from './pages/coaches/CoachesList.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import CoachRegister from './pages/coaches/CoachRegister.vue'
+import RequestReceived from './pages/requests/RequestReceived.vue'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [
+        { path: '/contact', component: ContactCoach }, // coaches/1/contact
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegister,
+    },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+})
+
+export default router
