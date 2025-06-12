488a81f5975c60bb890411269927e09f47caa20e
Cristian  Murillo <cam_725@hotmail.com>
2025-06-12 22:14:42 +0200

    implemented using stores for storing new instructors

--- 14-vue-main-project-challenge/src/pages/coaches/CoachRegistration.vue
+++ 14-vue-main-project-challenge/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,22 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveCoachData"></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+
+export default {
+  components: { CoachForm },
+  methods: {
+    saveCoachData(data) {
+      this.$store.dispatch('coaches/registerCoach', data)
+      this.$router.replace('/coaches')
+    },
+  },
+}
+</script>
--- 14-vue-main-project-challenge/src/pages/coaches/CoachesList.vue
+++ 14-vue-main-project-challenge/src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
     <base-card>
       <div class="controls">
         <base-button link mode="outline">Refresh</base-button>
-        <base-button link :to="'/register'">Register as coach</base-button>
+        <base-button v-if="!isCoach" link :to="'/register'">Register as coach</base-button>
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -42,6 +42,9 @@ export default {
     }
   },
   computed: {
+    isCoach() {
+      return this.$store.getters['coaches/isCoach']
+    },
     filteredCoaches() {
       const coaches = this.$store.getters['coaches/coaches']
       return coaches.filter((coach) => {
