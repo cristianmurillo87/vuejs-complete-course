4eb3167da2f3da213639430ecb3f17b8115afd6a
Cristian  Murillo <cam_725@hotmail.com>
2025-06-15 09:56:32 +0200

    display loading spinner while coaches data is being loaded

--- 14-vue-main-project-challenge/src/pages/coaches/CoachesList.vue
+++ 14-vue-main-project-challenge/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button link mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach" link :to="'/register'">Register as coach</base-button>
+        <base-button v-if="!isCoach && !isLoading" link :to="'/register'"
+          >Register as coach</base-button
+        >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach of filteredCoaches"
           :key="coach.id"
@@ -34,6 +39,7 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -64,7 +70,7 @@ export default {
     },
 
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches']
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches']
     },
   },
   created() {
@@ -74,8 +80,10 @@ export default {
     setFilters(filters) {
       this.activeFilters = filters
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches')
+    async loadCoaches() {
+      this.isLoading = true
+      await this.$store.dispatch('coaches/loadCoaches')
+      this.isLoading = false
     },
   },
 }
