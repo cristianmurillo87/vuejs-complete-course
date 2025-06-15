0d72bf3e30b1a558eff7fd5b4d422f82092152f1
Cristian  Murillo <cam_725@hotmail.com>
2025-06-15 19:42:57 +0200

    created not found page

--- 14-vue-main-project-challenge/src/pages/NotFound.vue
+++ 14-vue-main-project-challenge/src/pages/NotFound.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check all out
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
