ab8f958b251c2eb6fdf90ac00305ff55e911953b
Cristian  Murillo <cam_725@hotmail.com>
2025-06-22 15:20:44 +0200

    explored the context parameter withing the setup function

--- 15-vue-composition-api-basics/src/components/UserData.vue
+++ 15-vue-composition-api-basics/src/components/UserData.vue
@@ -1,6 +1,8 @@
 <template>
-  <h2>{{ userName }}</h2>
-  <h3>{{ age }}</h3>
+  <div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
 </template>
 
 <script>
@@ -8,10 +10,15 @@ import { computed } from 'vue'
 
 export default {
   props: ['firstName', 'lastName', 'age'],
-  setup(props) {
+  // the contex contains among others the attrs, emit and slots properties
+  // which can be used for emitting custom events
+  // and use the "slot" elements in the template
+  setup(props, context) {
     const userName = computed(() => {
       return props.firstName + ' ' + props.lastName
     })
+
+    console.log('context', context)
 
     return {
       userName,
