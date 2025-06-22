3f3649d847956a7ceaadf98626b7c3aa8ebe5903
Cristian  Murillo <cam_725@hotmail.com>
2025-06-22 14:38:23 +0200

    used computed with the composition API

--- 15-vue-composition-api-basics/src/App.vue
+++ 15-vue-composition-api-basics/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <h2>{{ fullName }}</h2>
     <h3>{{ user.age }}</h3>
     <button @click="setAge(37)">Change age</button>
+
+    <div>
+      <input type="text" placeholder="First name" @input="setFirstName" v-model="firstName" />
+      <input type="text" placeholder="Last name" @input="setLastName" v-model="lastName" />
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 
 export default {
   setup() {
     // ref can be used with aany data type
-    // const userName = ref('Cris')
-    // const userAge = ref(30)
+    const firstName = ref('Cristian')
+    const lastName = ref('Murillo')
 
     // reactive must always be passed and object
     const user = reactive({
@@ -21,11 +26,23 @@ export default {
       age: 30,
     })
 
+    const fullName = computed(() => {
+      return firstName.value + ' ' + lastName.value
+    })
+
     const setAge = (age) => {
       user.age = age
     }
 
-    return { user, setAge }
+    const setFirstName = (event) => {
+      firstName.value = event.target.value
+    }
+
+    const setLastName = (event) => {
+      lastName.value = event.target.value
+    }
+
+    return { user, firstName, lastName, fullName, setAge, setFirstName, setLastName }
   },
   // data() {
   //   return {
