300894e1c6ce90d1266ce793a77e7c6cdf5fa3c7
Cristian  Murillo <cam_725@hotmail.com>
2025-06-24 00:28:11 +0200

    created mixin and applied it to AddUser and DeleteUser components

---
+++ 18-vue-mixins/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import loggerMixin from './mixins/logger.js'
+
+import App from './App.vue'
+
+const app = createApp(App)
+app.mixin(loggerMixin)
+
+app.mount('#app')
